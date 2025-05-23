b6d79ee28334c15d5e7a1d0118d221c1dc1b084b
alessandroschiano98 <schianoski98@gmail.com>
2025-05-23 11:54:53 +0200

    inizializzare

---
+++ src/org/lessons/java/Evento.java
@@ -0,0 +1,53 @@
+package org.lessons.java;
+
+public class Evento {
+    private String titolo;
+    private int data;
+    private int  numeroPostiTotale;
+    private int numeroPostiPrenotati;
+
+public Evento(String titolo, int data, int numeroPostiTotale, int numeroPostiPrenotati){
+    this.titolo = titolo;
+    this.data = data;
+    this.numeroPostiTotale = numeroPostiTotale;
+    this.numeroPostiPrenotati = 0;
+}
+
+
+
+    public String getTitolo() {
+        return this.titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getNumeroPostiTotale() {
+        return this.numeroPostiTotale;
+    }
+
+    public void setNumeroPostiTotale(int numeroPostiTotale) {
+        this.numeroPostiTotale = numeroPostiTotale;
+    }
+
+    public int getNumeroPostiPrenotati() {
+        return this.numeroPostiPrenotati;
+    }
+
+    public void setNumeroPostiPrenotati(int numeroPostiPrenotati) {
+        this.numeroPostiPrenotati = numeroPostiPrenotati;
+    }
+
+
+
+
+}
